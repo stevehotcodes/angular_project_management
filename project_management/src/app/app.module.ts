562d85224ec950f686455032b9f34f3c86b29e56
Stephen Ondieki <stevehotcodes@gmail.com>
2023-11-08 13:22:51 +0300

    created the landing page

--- project_management/src/app/app.module.ts
+++ project_management/src/app/app.module.ts
@@ -4,21 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './component/login/login.component';
+// import { LoginComponent } from './component/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent,
+    
     NavbarComponent,
     FooterComponent,
     AdminDashboardComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
